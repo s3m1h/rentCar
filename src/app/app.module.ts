72ee6b14500f7ba70af12ceea22d9c717f98c88f
s3m1h <semih_acar01@hotmail.com>
2022-03-27 12:11:29 +0300

    rentCar odev 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CarComponent } from './components/car/car.component';
+import { NaviComponent } from './components/navi/navi.component';
+import { CategoryComponent } from './components/category/category.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarComponent,
+    NaviComponent,
+    CategoryComponent
   ],
   imports: [
     BrowserModule,
